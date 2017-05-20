7463bd0a791d0322fb9c6fb06ac74f42f74bae25
actanble <actanble@163.com>
2017-05-20 18:15:42 +0800

    second add -date-need-->id

--- TWFB/mainapp/urls.py
+++ TWFB/mainapp/urls.py
@@ -4,7 +4,9 @@ from . import views
 app_name = 'mainapp'
 urlpatterns = [
     url(r'^$', views.home, name='index_base'),
-    url(r'^zblist$', views.zbList, name="zbList"),
+    #url(r'^zblist$', views.zbList, name="zbList"),
     url(r'^h$', views.history, name='history'),
     url(r'^actanble$', views.test_ajax, name='test_ajax'),
+    url(r'^test_list$', views.test_list, name='text_list'), ## buttom test
+    url(r'^new_append$', views.new_append, name="ul_new_append"),
 ]
--- TWFB/mainapp/views.py
+++ TWFB/mainapp/views.py
@@ -6,12 +6,6 @@ from django.http import HttpResponse
 from .models import Item,LastPostID
 from datetime import datetime
 
-class Res:
-    def __init__(self):
-        self.realname = ""
-        self.content = ""
-
-
 ## init-need now is no-use
 def zbList(request):
     #if not request.is_ajax():
@@ -38,8 +32,9 @@ def zbList(request):
     return JsonResponse(name_dict)
 
 def history(request):
-    name_dict = {}
     lst = Item.objects.all()
+    length = len(lst)
+    LastPostID.objects.create(postdt=datetime.today(), postid=length + 1)
     # set the zblist
     zblist = []
     for x in lst:
@@ -47,6 +42,7 @@ def history(request):
         i_dir.setdefault("realname", x.realname)
         i_dir.setdefault("content", x.content)
         zblist.append(i_dir)
+
     return render(request, "resource.html", {"zblist":zblist})
 
 def add_ls(request):
@@ -66,9 +62,7 @@ def add_ls(request):
     return render(request, "ul.html", {"add_ls":add_ls, "test":test})
 
 def home(request):
-
     test = "test"
-
     return render(request, "index.html", {"t":test})
 
 def test_ajax(request):
@@ -77,4 +71,42 @@ def test_ajax(request):
         {"optionKey": "2", "optionValue": "Wind Song"},
         {"optionKey": "3", "optionValue": "Wings"}
     ]
-    return JsonResponse({"dict":dict})
+    return JsonResponse({"dict": dict})
+
+def test_list(request):
+    name_dict = {}
+    lst = Item.objects.all()
+    # set the zblist
+    zblist = []
+    for x in lst:
+        i_dir = {}
+        i_dir.setdefault("realname", x.realname)
+        i_dir.setdefault("content", x.content)
+        zblist.append(i_dir)
+    return JsonResponse({"value":zblist})
+
+def new_append(request):
+    if LastPostID.objects.all() is None:
+        LastPostID.objects.create(postdt = datetime.today(), postid = 1)
+    length = len(LastPostID.objects.all())
+    last = LastPostID.objects.all()[length - 1]
+    last_dt = last.postdt
+    print(last_dt)
+    lst = [x for x in Item.objects.all() if x.dt > last_dt] # add_need of the data from database
+    # sign the date that have add
+    add_ls = []
+    flag  = False
+    for x in lst:
+        i_dir = {}
+        i_dir.setdefault("realname", x.realname)
+        i_dir.setdefault("content", x.content)
+        add_ls.append(i_dir)
+        flag = True
+    if flag:
+        print ("不是空, 进去添加过元素")
+        LastPostID.objects.create(postdt=datetime.today(), postid=length + 1)
+    print(add_ls)
+    
+    return JsonResponse({"value":add_ls})
+
+
